5d9cb18a10e0881aac63be5214cbcd1a499ae795
Konstantin Ketterer <konstantin.ketterer@student.hpi.de>
2025-10-20 16:46:57 +0200

    Add ruff

--- src/usigrabber/__init__.py
+++ src/usigrabber/__init__.py
@@ -1,8 +1,12 @@
 from test_package import main as test_package_main
 
+
 def main() -> None:
-    print("Hello from usigrabber!")
+    print(
+        "Hello from usigrabber! Hello from usigrabber!Hello from usigrabber!Hello fromf"
+    )
     test_package_main()
+
 
 if __name__ == "__main__":
     main()
